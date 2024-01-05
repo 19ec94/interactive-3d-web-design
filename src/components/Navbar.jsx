c7ecd6cd871387badd6f09d52a06757334a3ffe5
AlkhazovAvdalim <avdalim@outlook.com>
2024-01-05 17:53:17 +0100

    Added x on Menu, resized logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,44 @@
 import React, { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import "./navbar.css";
+import logo from "../Assets/logo.jpeg";
+import x from "../Assets/x-mark-24.png";
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav>
       <Link to="/" className="title">
-        Interactive 3D
+        <img id="logo" src={logo} alt="" />
       </Link>
-      <div
-        className="menu"
-        onClick={() => {
-          setMenuOpen(!menuOpen);
-          console.log();
-        }}
-      >
+
+      {/* <span></span>
         <span></span>
-        <span></span>
-        <span></span>
-      </div>
+        <span></span> */}
+      {menuOpen === false ? (
+        <div
+          className="menu"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+            console.log();
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      ) : (
+        <div
+          className="menu"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+            console.log();
+          }}
+        >
+          <img src={x} id="x"></img>
+        </div>
+      )}
+
       <ul className={menuOpen ? "open" : ""}>
         <li>
           <NavLink to="/about">About</NavLink>
