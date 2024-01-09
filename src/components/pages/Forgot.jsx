d28e15d444046fa480ed620334ba49f16fc1c663
19ec94 <bert.edil@gmail.com>
2024-01-09 15:01:17 +0100

    Simplify reset password operation
    To follow the same approach used in login and signup forms, the
    following changes are proposed:
      - add the custom FormElement for email field
      - remove `action` variable to capture the state. Instead, create a
        boolean-type variable `isSubmitted` to keep track of the submission state.
      - in case of no email received, direct the user back to reset page by
        providing the same link to original reset page.

--- src/components/pages/Forgot.jsx
+++ src/components/pages/Forgot.jsx
@@ -1,53 +1,86 @@
 import React, { useState } from "react";
 import email_icon from "../../Assets/email.png";
 import { Link } from "react-router-dom";
+import FormElement from "./FormElement"
 
 export const Forgot = () => {
-  const [action, setAction] = useState("Forgot Password");
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const [formData, setFormData] = useState({
+    user_email: '',
+  });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, 
+      [event.target.name]: event.target.value });
+  };
+
+
+  const handleSubmit = async (event) => {
+
+    event.preventDefault();
+
+    // define form submission logic here
+    console.log("Reset login credentials submitted!");
+
+    setIsSubmitted(true);
+  };
+
+  const handleRetryReset = () => {
+    setIsSubmitted(false);
+  };
+
   return (
     <div className="container">
-      <div className="header">
-        <div className="text">{action}</div>
-        <div className="underline"></div>
-      </div>
-      <div className="inputs">
-        {action === "Forgot Password" ? (
+      <form onSubmit={handleSubmit}>
+        <div className="header">
+          { isSubmitted ? (
+            <div className="text">An Email has been sent!</div>
+          ): (
+            <div className="text">Forgot Login details?</div>
+          )}
+          <div className="underline"></div>
+        </div> 
+        { isSubmitted ? (
           <div className="inputs">
-            <div className="input">
-              <img src={email_icon} alt="" />
-              <input type="email" placeholder="Email" />
-            </div>
-            <div className="submit-container-reset">
-              <div
-                className={
-                  action === "Forgot Password" ? "submit" : "submit hidden"
-                }
-                onClick={() => {
-                  setAction("Email Sent Out");
-                }}
-              >
-                Reset Password
+            <div className="link-to-container">
+              <div className="link-to-signup">
+                Found your login details? Proceed to {" "}
+                <Link to="/login" 
+                >
+                Log in</Link>
+              </div>
+              <div className="link-to-reset-data">
+                Haven't received the Email yet? {" "}
+                <Link to="/forgot"
+                onClick={handleRetryReset}
+                >
+                Retry reset</Link>
               </div>
             </div>
           </div>
         ) : (
           <div className="inputs">
-            <div className="password-submitted">
-              Reset Submitted! Check Emails
-            </div>
-            <div className="submit-container-reset">
-              <div
-                className="submit"
-                onClick={() => {
-                  window.location.reload();
-                }}
+            <FormElement
+              imgSrc={email_icon}
+              inputType="email"
+              inputName="user_email"
+              inputValue={formData.user_email}
+              inputPlaceholder="Enter your Email"
+              inputOnChange={handleChange}
+            />
+            <div className="submit-container">
+              <button 
+                type="submit" 
+                className="submit" 
               >
-                Retry
-              </div>
+                Reset
+              </button>
             </div>
           </div>
         )}
-      </div>
-    </div>
+      </form>
+    </div> 
   );
 };
