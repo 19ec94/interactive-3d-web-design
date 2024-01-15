db1f3391d0bc02957561ce04363a7a0c949d9c66
19ec94 <bert.edil@gmail.com>
2024-01-15 13:31:09 +0100

    Render components based on login status
    Display /, /About, /Login routes if the user is not logged in
    Display all the pages if the user is logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,23 @@ import "./navbar.css";
 import logo from "../Assets/logo.jpeg";
 import x from "../Assets/x-mark-24.png";
 
+import { useAuth } from './AuthContext';
+
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  // Define a variable to store login status. If logged in, display all
+  // navigation link, else display only about, login links.
+  const {isLoggedIn} = useAuth();
+
+  console.log('navbar element', isLoggedIn);//Debugging
+
   return (
     <nav>
       <Link to="/" className="title">
         <img id="logo" src={logo} alt="" />
       </Link>
 
-      {/* <span></span>
-        <span></span>
-        <span></span> */}
       {menuOpen === false ? (
         <div
           className="menu"
@@ -35,10 +41,11 @@ export const Navbar = () => {
             console.log();
           }}
         >
-          <img src={x} id="x"></img>
+          <img src={x} id="x" alt=""></img>
         </div>
       )}
 
+        {  isLoggedIn ? (
       <ul className={menuOpen ? "open" : ""}>
         <li>
           <NavLink to="/about">About</NavLink>
@@ -53,6 +60,17 @@ export const Navbar = () => {
           <NavLink to="/scoreboard">Scoreboard</NavLink>
         </li>
       </ul>
+        ) : (
+      <ul className={menuOpen ? "open" : ""}>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
+        </li>
+      </ul>
+        )
+        }
     </nav>
   );
 };
