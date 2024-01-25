d75499c6efb619c136e57f0fea64b135387cf109
19ec94 <bert.edil@gmail.com>
2024-01-25 15:21:55 +0100

    Add dashboard page and its style

---
+++ src/components/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Dashboard.css";
+
+export const Dashboard = () => {
+  return <div className="dashboard-container">
+    <div className="header">
+      <div className="text">
+        Welcome To Dashboard!
+      </div>
+    </div>
+    </div>;
+};
