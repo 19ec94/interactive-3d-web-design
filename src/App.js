1a97546c4952e1a3dd9b8f91b6e6f87fb6670fb0
19ec94 <bert.edil@gmail.com>
2024-01-25 16:45:45 +0100

    Re-order import statements alphabatically

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import "./App.css";
 import { Navbar } from "./components/Navbar";
 import {Route, Routes } from "react-router-dom";
 
+import {About} from './components/About';
+import {AuthProvider} from './components/AuthContext';
+import {Dashboard} from './components/Dashboard';
 import {Forgot} from './components/Forgot';
-import {Scoreboard} from './components/Scoreboard';
 import {Home} from './components/Home';
 import {Levels} from './components/Levels';
-import {Dashboard} from './components/Dashboard';
 import {Login} from './components/Login';
-import {About} from './components/About';
-import {Signup} from './components/Signup';
-import {AuthProvider} from './components/AuthContext';
 import {ProtectedRoute} from "./components/ProtectedRoute";
+import {Scoreboard} from './components/Scoreboard';
+import {Signup} from './components/Signup';
 
 function App() {
   return (
