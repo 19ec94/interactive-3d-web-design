0be512eeb5f8f952afbc380b8daed950ae7cea09
19ec94 <bert.edil@gmail.com>
2024-02-02 18:04:33 +0100

    Simplify global data and sessionStorage
    Rationale:
     - collect data related to each session into one varibale - sessionData.

--- src/components/AuthContext.jsx
+++ src/components/AuthContext.jsx
@@ -5,43 +5,22 @@ const AuthContext = createContext();
 
 // 2. Create a provider component to provide values to children component
 export const AuthProvider = ({ children }) => {
-  // Define variable to hold login status
-  const [isLoggedIn, setIsLoggedIn] = useState( () => {
-    const storedStatus = sessionStorage.getItem('isLoggedIn');
-    return storedStatus ? JSON.parse(storedStatus) : false;
+  // Define varibale to hold session data
+  // NOTE: This data will be displayed in dashboard
+  const [sessionData, setSessionData] = useState({
+    isLoggedIn: false,
+    sessionToken: '',
+    userName: '',
+    userEmail: ''
   });
-  // Define varibale to hold session token
-  const [sessionToken, setSessionToken] = useState( () => {
-    const storedToken = sessionStorage.getItem('sessionToken');
-    return storedToken ? JSON.parse(storedToken) : '';
-  });
- 
-  // Define a function to update global variables upon login
-  // TODO: Find a better way to set and remove global variables
-  const login = () => {
-    setIsLoggedIn(true);
-    sessionStorage.setItem('isLoggedIn', JSON.stringify(true));
-  };
 
-  // Define a function to update global variables upon logout
-  // TODO: Find a better way to set and remove global variables upon login
-  const logout = () => {
-    setIsLoggedIn(false);
-    sessionStorage.removeItem('isLoggedIn');
-  };
-  
-  // Define a function to update the global varibale within a child component
-  const setLoginStatusGlobally = (status) => {
-    setIsLoggedIn(status);
-    sessionStorage.setItem('isLoggedIn', JSON.stringify(status));
-  };
-  const setSessionTokenGlobally = (sessionToken) => {
-    setSessionToken(sessionToken);
-    sessionStorage.setItem('sessionToken', JSON.stringify(sessionToken));
+  const setSessionDataGlobally = (sessionData) => {
+    setSessionData(sessionData);
+    sessionStorage.setItem('sessionData', JSON.stringify(sessionData));
   };
 
   return (
-    <AuthContext.Provider value={{isLoggedIn, sessionToken, login, logout, setLoginStatusGlobally, setSessionTokenGlobally}}>
+    <AuthContext.Provider value={{sessionData, setSessionDataGlobally}}>
       { children }
     </AuthContext.Provider>
   );
