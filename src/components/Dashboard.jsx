a5d53be31c99e7192303dbc4733ffd7bb5d3c5a4
19ec94 <bert.edil@gmail.com>
2024-02-02 17:59:41 +0100

    Simplify dashboard
    Rationale:
     - Dashboard is going to content several sections in the future.
       Therefore, following react priciple, create seperate files for each
       comoponent.
    This commit will
     - remove profile code from dashboard
     - import Profile react component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
 import "./Dashboard.css";
+import  { Profile } from "./Profile";
 
 export const Dashboard = () => {
-  return <div className="dashboard-container">
-    <div className="header">
-      <div className="text">
-        Welcome To Dashboard!
+  return (
+    <div className="dashboard">
+
+      <div className="dashboard__header">
+        <div className="dashboard__title">
+          Dashboard
+        </div>
+        <div className="dashboard__underline">
+        </div>
       </div>
+
+      {/* Profile component */}
+      <Profile />
+
+      {/* TODO Game component */}
+
     </div>
-    </div>;
+  );
 };
