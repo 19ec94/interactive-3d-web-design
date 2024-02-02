9d6f4debcd278bf3bc3c70f8160d2ea927ab00fd
19ec94 <bert.edil@gmail.com>
2024-02-02 17:58:47 +0100

    Create a react component for Profile in dashboard

---
+++ src/components/Profile.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faLock, faUser } from '@fortawesome/free-solid-svg-icons'
+
+import "./Profile.css"
+
+export const ProfileField = ({ labelContent, value, onUpdate }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedValue, setEditedValue] = useState(value);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleUpdateClick = () => {
+    onUpdate(editedValue);// send data to server to be updated
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="profile-field">
+      <label>{labelContent}</label>
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={editedValue}
+            onChange={(e)=>setEditedValue(e.target.value)}
+          />
+          <button onClick={handleUpdateClick}>Update</button>
+        </>
+      ) : (
+        <>
+          <span>{editedValue}</span>
+          <button onClick={handleEditClick}>Edit</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export const Profile = () => {
+  // get the user data from sessionStorage
+  const [userName, setUserName] = useState("X");
+  const [userEmail, setUserEmail] = useState("1@gmail.com");
+  const [userPassword, setUserPassword] = useState("*****");
+
+  const handleUserNameUpdate = (newUserName) => {
+    //TODO: Send DATA to server
+    setUserName(newUserName);
+  };
+  const handleUserEmailUpdate = (newUserEmail) => {
+    //TODO: Send DATA to server
+    setUserEmail(newUserEmail);
+  };
+  const handleUserPasswordUpdate = (newUserPassword) => {
+    //TODO: Send DATA to server
+    setUserPassword(newUserPassword);
+  };
+  return (
+    <div className="profile">
+
+      <div className="title">
+        Profile
+      </div>
+
+      <div className="profile-fields">
+        <ProfileField 
+          labelContent={<FontAwesomeIcon icon={ faUser }/>}
+          value={userName}
+          onUpdate={handleUserNameUpdate}
+        />
+        <ProfileField 
+          labelContent={ <FontAwesomeIcon icon={ faEnvelope} /> }
+          value={userEmail}
+          onUpdate={handleUserEmailUpdate}
+        />
+        <ProfileField 
+          labelContent={ <FontAwesomeIcon icon={ faLock } /> }
+          value={userPassword}
+          onUpdate={handleUserPasswordUpdate}
+        />
+      </div>
+
+    </div>
+  );
+}
