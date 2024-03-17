f9d02d0b563def8b4d66e0fb3769d9a1fa5b183f
rmorasis <rebemm24@gmail.com>
2024-03-17 20:34:25 +0100

    Add Buttons and logo to Homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Home.css"
 
 export const Home = () => {
-  return <div>Home</div>;
+  return <div>
+  <div className="img-center"><img src="/src/Assets/logo.jpeg" width={"250px"} /></div>
+  <div className="button-container">
+    <div className="custom-li"><NavLink to="/login"><button type="button" className="custom-button">Login</button></NavLink> </div>
+    <div className="custom-li"><NavLink to="/about"><button type="button" className="custom-button">About</button></NavLink></div>
+    <div className="custom-li"><NavLink to="/scoreboard"><button type="button" className="custom-button">Scoreboard</button></NavLink></div>
+
+  </div>
+  </div>;
 };
